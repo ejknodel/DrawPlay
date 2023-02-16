d23870591fe1db8ea418c2b884d24c4c02087c96
swilliams <smwilliams@mines.edu>
2023-02-16 14:42:02 -0700

    changed the cat's cat(ch phrase) haha to 10101010

--- src/Cat.java
+++ src/Cat.java
@@ -5,7 +5,7 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - no one needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 300;
+	private static final int HEAD_DIMENSION = 300;                  // changed from 100 to 300
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("10101010", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
